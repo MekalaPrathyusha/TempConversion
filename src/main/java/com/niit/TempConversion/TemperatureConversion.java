90b0776ad4ff0ca561642d0d96dbf03463b65fce
MekalaPrathyusha <prathyu595@gmail.com>
2017-04-27 15:08:31 +0530

    Method name
    Java Convention

--- src/main/java/com/niit/TempConversion/TemperatureConversion.java
+++ src/main/java/com/niit/TempConversion/TemperatureConversion.java
@@ -1,11 +1,11 @@
 package com.niit.TempConversion;
 
 public class TemperatureConversion {
-	public static double farenheitTocelcius(double farenheit){
+	public static double farenheitToCelcius(double farenheit){
 		return(farenheit-32)*5/9;
 	}
-	public static double celsiusTofarenheit(double celsius){
-		return (celsius*9/5)+32;
+	public static double celsiusTofarenheit(double Celsius){
+		return (Celsius*9/5)+32;
 		
 	}
 
