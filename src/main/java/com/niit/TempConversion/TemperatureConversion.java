9da6414f940ef490084337d5c3fe1a4f0ab95bf1
MekalaPrathyusha <prathyu595@gmail.com>
2017-04-27 15:01:25 +0530

    Temperature conversion
    temperature

---
+++ src/main/java/com/niit/TempConversion/TemperatureConversion.java
@@ -0,0 +1,12 @@
+package com.niit.TempConversion;
+
+public class TemperatureConversion {
+	public static double farenheitTocelcius(double farenheit){
+		return(farenheit-32)*5/9;
+	}
+	public static double celsiusTofarenheit(double celsius){
+		return (celsius*9/5)+32;
+		
+	}
+
+}
